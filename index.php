dccecb1e7937faec20e7396aa5f1ad9e5baf0cc0
saaibyts <saaibytsvgs@gmail.com>
2019-03-29 13:14:59 +0100

    Lagde index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<link rel="stylesheet" href="hovedside.css">
+</head>
+<?php
+
+session_start();
+
+if(!$_SESSION["auth"])
+{
+	header("location:testlogin4.php");
+}
+
+require 'kobling.php';
+
+?>
+<html>
+<head>
+<link rel="stylesheet" href="hovedside.css">
+</head>
+
+<body>
+<div class="container">
+<h1> Main Page </h1>
+
+<ul>
+<div class="valg">
+<div class="meny">
+	<li><a href="logg_main.php"><img src="log1.png" class="responsive"></a></li>
+		<h2> Log </h2>
+		Log your lifts to see your progression
+</div>
+<div class="meny">
+	<li><a href="kalkulator.php"><img src="kalkuler.png" class="responsive"></a></li>
+		<h2> Calculate </h1>
+		Chose to calculate either macros or calories
+</div>
+</div>
+</ul>
+
+</div>
+</body>
+</html>
