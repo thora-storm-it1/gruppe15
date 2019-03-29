fe9cf27ad15ab4b9c365f76eb7b09b99d1fcb7b3
saaibyts <saaibytsvgs@gmail.com>
2019-03-29 21:10:04 +0100

    Lagde logg

---
+++ log.php
@@ -0,0 +1,73 @@
+<?php
+
+session_start();
+
+if(!$_SESSION["auth"])
+{
+	header("location:testlogin4.php");
+}
+
+if(isset($_POST["leggtil"]))
+{
+require 'kobling.php';
+
+$vekt = $_POST["vekt"];
+$repetitisjoner = $_POST["repetisjoner"];
+$bruker = $_SESSION["bruker_id"];
+$oevelse_id = $_POST["oevelse_id"];
+$dato = $_POST["dato"];
+
+$query = "INSERT INTO logg (vekt, repetisjoner, oevelse_id, bruker_id, dato) VAlUES ('$vekt', '$repetitisjoner', '$oevelse_id', '$bruker', '$dato')";
+	
+if($kobling->query($query))
+	{
+		header("location:hovedside.php");
+	}
+else
+{
+	echo "Noe gikk galt med spørringen $query ($kobling->error)";
+}
+}
+?>
+	<html>
+	<head>
+	<meta charset="UTF-8">
+	<title> Log </title>
+	</head>
+
+	<body>
+
+	<h1> Log </h1>
+	<form method="POST">
+	<input type="number" name="vekt" placeholder="Weight">
+	
+	<input type="number" name="repetisjoner" placeholder="Repetitions">
+	
+	<input type="datetime-local" name="dato">
+
+<?php
+
+require 'kobling.php';
+
+$sql = "SELECT * FROM oevelse";
+$resultat = $kobling->query($sql);
+
+echo "<select name='oevelse_id'>";
+
+while($rad = $resultat->fetch_assoc())
+{
+$oevelse_id = $rad["oevelse_id"];
+$oevelse = $rad["oevelse"];
+	
+	echo "<option value='$oevelse_id'> $oevelse </option>";	
+}	
+echo "</select>";	
+
+?>
+	
+	<input type="submit" name="leggtil" value="Add">
+	
+</form>
+
+</body>
+</html>
